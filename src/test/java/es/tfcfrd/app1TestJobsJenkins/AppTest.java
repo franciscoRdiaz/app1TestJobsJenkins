1ab9742966b8eb0a375eb5ba88c96dc4804e22a4
frdiaz <francisco.diazbayon@gmail.com>
2016-12-05 19:10:01 +0100

    Try Git from eclipse 1

---
+++ src/test/java/es/tfcfrd/app1TestJobsJenkins/AppTest.java
@@ -0,0 +1,44 @@
+package es.tfcfrd.app1TestJobsJenkins;
+
+import java.util.Random;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public AppTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( AppTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        int i = 0;
+        Random rnd = new Random();
+        i = rnd.nextInt(10);
+        System.out.println("Valor generado:"+i);
+    	assertTrue( i<5 );
+    }
+}
